9a19bdfa2b7525a0de041882c752e0a7a7f3eeed
AuroraXue <jx2291@columbia.edu>
2017-04-26 12:20:30 -0400

    shiny app framework

--- shiny/app.R
+++ shiny/app.R
@@ -1,38 +1,101 @@
 library(shiny)
 library(shinydashboard)
+library(omdbapi)
+library(dplyr)
+library(rvest)
+library(RCurl)
+library(jpeg)
+library(shinyBS)
 
 ui <- dashboardPage(
   dashboardHeader(title = "Movie Recommend"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Recommend by Movie Name", tabName = "MovieSearch", icon = icon("film")),
-      menuItem("Recommend by Type", icon = icon("th"), tabName = "widgets")),
-    menuItem("Top Movies", icon = icon("bar-charts"), tabName = "top")
-    )
+      menuItem("About This App",tabName = "info", icon=icon("id-card")),
+      menuItem("Top Movie",tabName = "top",icon = icon("thumbs-up")),
+      menuItem("Recommend by Type", icon = icon("th"), tabName = "words"),
+      menuItem("Recommend by Movie Name", tabName = "MovieSearch", icon = icon("film"))
+      
+    ))
   ,
   dashboardBody(
-    
+     tabItems(
+       tabItem(tabName = "info",h2("About this app:...")),
+       tabItem(tabName = "top",fluidRow(
+         box(title = "Top 1", 
+             background = "red",
+             status = "danger", 
+             width = 3,
+             solidHeader = TRUE,
+             plotOutput("top1", height = 250)),
+         box(title = "Top 2", 
+             background = "red",
+             status = "danger", 
+             width = 3,
+             solidHeader = TRUE,
+             plotOutput("top2", height = 250)),
+         box(title = "Top 3",
+             background = "red",
+             width = 3,
+             status = "danger",
+             plotOutput("top3"), height = 250),
+         box(title = "Top 4",
+             background = "red",
+             width = 3,
+             status = "danger",
+             plotOutput("top4"),height = 250),
+         box(title = "Top 50",
+             status = "primary",
+             solidHeader = TRUE,
+             collapsible = TRUE,
+             verbatimTextOutput("top50")),
+         box(title = "Popular but bad movies",
+             status = "success",
+             solidHeader = T,
+             verbatimTextOutput("pop_lrated")),
+         box(title = "Not popular but great movies",
+             status = "danger",
+             solidHeader = T,
+             verbatimTextOutput("nopop_hrated")) )),
+       tabItem(tabName = "words",h2("Mercury's")),
+       tabItem(tabName = "MovieSearch", 
+               fluidRow(
+               sidebarSearchForm(textId = "searchText", 
+                                 buttonId = "searchButton",
+                                 label = "Search the Movie..."))
+               )
+     )
 
       
-        fluidRow(
-          box(
-          title = "Inputs", solidHeader = TRUE,
-          "Box content here", br(), "More box content",
-          sliderInput("slider", "Slider input:", 1, 100, 50),
-          textInput("text", "Text input:")
-        )
-      )
-        
-    ,
-      
-      tabItem(tabName = "widgets",
-              h2("Widgets tab content")
-      )
-    
-  )
+    )
 )
+
+
+
+
+
+
+
   
 
-server <- function(input, output) { }
+server <- function(input, output) { 
+  output$tiles <- renderUI({
+    fluidRow(
+      lapply(top50_df$poster[1:8], function(i) {
+        a(box(width=3,
+              title = img(src = i, height = 350, width = 250),
+              footer = top50_df$top50[top50_df$poster == i]
+        ), href= top50_df$imbdLink[top50_df$poster == i] , target="_blank")
+      }) ##end of lappy
+    )
+  })##end of renderUI
+  
+  output$top50 <- renderText({top50_s })
+  output$pop_lrated <- renderText({pop_lrated_s})
+  output$nopop_hrated <- renderText({nopop_hrated_s})
+  
+  
+  
+  }
 
 shinyApp(ui, server)
